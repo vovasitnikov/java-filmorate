a0f21b74dd24a1d1ce2f65be3fb0f69618613fa5
Vova <vovasitnikov1209@yandex.ru>
2023-04-18 22:04:27 +0400

    yyy

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -7,29 +7,10 @@ import java.util.Collection;
 
 public interface FilmService extends Service<Film> {
 
-    /**
-     * Метод возвращает коллекцию состоящую из N
-     * фильмов по популярности, основываясь на
-     * количестве лайков у каждого фильма.
-     *
-     * @param count размер коллекции.
-     * @return коллекция популярных фильмов.
-     */
+
     Collection<Film> getPopularFilms(int count);
 
-    /**
-     * Метод добавляет пользовательский лайк фильму.
-     *
-     * @param filmID идентификатор фильма.
-     * @param userID идентификатор пользователя.
-     */
     void addLike(long filmID, long userID);
-
-    /**
-     * Метод удаляет пользовательский лайк у фильма.
-     *
-     * @param filmID идентификатор фильма.
-     * @param userID идентификатор пользователя.
-     */
+    
     void deleteLike(long filmID, long userID);
 }
